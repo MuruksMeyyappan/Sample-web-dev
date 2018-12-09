a52ace7a396954662c5e51280e59daaea2d502e0
Muruks <muruksmroger@gmail.com>
2018-12-09 17:10:16 +0530

    Added routers files and install hapi js

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 var express = require('express');
 var app = express();
-var path = require('path');
- 
-app.use('/cssFiles',express.static(__dirname + '/styles'));
-app.use('/image',express.static(__dirname +  '/image'));
-app.get('/', function(req, res) {
-    res.sendfile('HomePage.html', {root : path.join(__dirname,'./src')});
-});
 
+var routers = require('./routers.js');
+
+
+app.use('/',express.static(__dirname + '/styles'));
+app.use('/',express.static(__dirname +  '/image'));
+app.use('/',express.static(__dirname + '/scripts'))
+// app.all('/', function(req, res) {
+//     res.sendfile('HomePage.html', {root : path.join(__dirname,'./src')});
+//     console.log("server refereshedharry_pregular");
+// });
+// app.post('/hello', function(req, res){
+//         res.send("You just called the post method at '/hello'!\n");
+//      });
+app.use('/Homepage',routers)
 app.listen(8000);
--- scripts/firstJS.js
+++ scripts/firstJS.js
@@ -1,1 +1,19 @@
-document.querySelector('.heading').textContent = "BIRA";
+var myBtn = document.querySelector('button');
+var myHeading = document.querySelector('.mesg')
+
+function SetUserName (){
+    var myName = prompt('pls enter the name');
+    localStorage.setItem('name',myName);
+    myHeading.textContent = "y fucked up man" + myName;
+}
+
+if(!localStorage.getItem('name')){
+    SetUserName();
+}else {
+    var storedData = localStorage.getItem('name');
+    myHeading.textContent =  "y fucked up man" +" " + storedData;
+}
+
+myBtn.onclick = function(){
+    SetUserName();
+}
